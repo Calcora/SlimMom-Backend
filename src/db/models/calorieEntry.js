71548a0d37f6d9b7bf97546ef02636f865182076
Özge SEVİM <ozgsvm@gmail.com>
2025-09-25 21:25:40 +0300

    search query  files added

---
+++ src/db/models/calorieEntry.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const calorieEntrySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    left: { type: Number, required: true },
+    consumed: { type: Number, required: true },
+    dailyRate: { type: Number, required: true },
+    nOfNormal: { type: Number, required: true },
+    notAllowedProducts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    ],
+    createdAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+const CalorieEntry = mongoose.model("CalorieEntry", calorieEntrySchema);
+
+export default CalorieEntry;
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,5 +1,11 @@
 import Product from "../db/models/product.js";
+import { parseFilterParams } from "../utils/parseFilterParams.js";
 
 export const getProducts = async () => {
   return await Product.find();
+};
+
+export const filteredProducts = async (query) => {
+  const filter = parseFilterParams(query);
+  return await Product.find(filter);
 };
