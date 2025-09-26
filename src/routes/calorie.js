d346b6bb54c71f84f0d1de1db507b4bf0b15f68e
Mürsel Şen <46069077+murselsen@users.noreply.github.com>
2025-09-26 21:43:43 +0300

    Revert "Add public calorie entry"

--- src/routes/calorie.js
+++ src/routes/calorie.js
@@ -1,14 +1,9 @@
 import express from "express";
-import {
-  privateCalorieEntryController,
-  publicCalorieEntryController,
-} from "../controllers/calorieController.js";
+import { privateCalorieEntryController } from "../controllers/calorieController.js";
 import { verifyToken } from "../middlewares/auth.js";
 
 const calorieRouter = express.Router();
 
 calorieRouter.post("/private", verifyToken, privateCalorieEntryController);
-
-calorieRouter.post("/public", publicCalorieEntryController);
 
 export default calorieRouter;
--- src/services/calorieService.js
+++ src/services/calorieService.js
@@ -3,13 +3,12 @@ import Product from "../db/models/product.js";
 
 export const privateCalorieEntry = async (user, userData) => {
   const { height, age, currentWeight, desiredWeight, bloodType } = userData;
-  const dailyRate = Math.ceil(
+  const dailyRate =
     10 * currentWeight +
-      6.25 * height -
-      5 * age -
-      161 -
-      10 * (currentWeight - desiredWeight)
-  );
+    6.25 * height -
+    5 * age -
+    161 -
+    10 * (currentWeight - desiredWeight);
 
   const allProducts = await Product.find();
   const notAllowedProducts = allProducts.filter(
@@ -17,37 +16,13 @@ export const privateCalorieEntry = async (user, userData) => {
   );
 
   const calorieEntry = await CalorieEntry.create({
+    notAllowedProducts: notAllowedProducts.map((p) => p._id),
     dailyRate,
     userId: user._id ?? user.id,
     consumed: 0,
     left: dailyRate,
     nOfNormal: 0,
-    notAllowedProducts: notAllowedProducts.map((p) => p._id),
   });
 
   return calorieEntry;
-};
-
-export const publicCalorieEntry = async (userData) => {
-  const { height, age, currentWeight, desiredWeight, bloodType } = userData;
-  const dailyRate = Math.ceil(
-    10 * currentWeight +
-      6.25 * height -
-      5 * age -
-      161 -
-      10 * (currentWeight - desiredWeight)
-  );
-
-  const allProducts = await Product.find();
-  const notAllowedProducts = allProducts.filter(
-    (product) => product.groupBloodNotAllowed[bloodType]
-  );
-
-  return {
-    dailyRate,
-    consumed: 0,
-    left: dailyRate,
-    nOfNormal: 0,
-    notAllowedProducts: notAllowedProducts.map((p) => p._id),
-  };
 };
